5c1e29ddd488d9f357dd6263f0489f07ea886214
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-01-16 02:55:59 +0200

    create FinancierSchema

---
+++ backend/models/Financier.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const Schema = mongoose.Schema;
+
+const FinancierSchema = new Schema({
+	name: {
+		type: String,
+		required: [true, 'name is required'],
+	},
+	userName: {
+		type: String,
+		required: [true, 'userName is required'],
+	},
+	email: {
+		type: String,
+		unique: true,
+		required: [true, 'email is required'],
+	},
+	nationalID: {
+		type: Number,
+		unique: true,
+		required: [true, 'national ID is required'],
+		minlength: [6, 'Please enter password min length is 6'],
+	},
+	password: {
+		type: String,
+		required: [true, 'password is required'],
+		minlength: [6, 'Please enter password min length is 6'],
+	},
+	fileNum: {
+		type: String,
+		required: [true, 'file number is required'],
+	},
+	TaxRegistrationNum: {
+		type: String,
+		required: [true, 'file number is required'],
+	},
+});
+
+UserSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('Financier', UserSchema);
