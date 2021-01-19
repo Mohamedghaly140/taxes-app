259fdfeaf905537326c3bac8e60b5031a379dfcd
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-01-19 20:58:51 +0200

    setup frontend

---
+++ frontend/src/router/Router.js
@@ -0,0 +1,11 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const Routes = () => {
+	return (
+		<BrowserRouter>
+			<Switch></Switch>
+		</BrowserRouter>
+	);
+};
+
+export default Routes;
